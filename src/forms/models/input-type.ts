0d05bbd5893c4f7684d12cf479ab550220747c0c
Adrian Lemes <>
2020-04-18 19:33:10 -0300

    showing HTML with form-control

--- src/forms/models/input-type.ts
+++ src/forms/models/input-type.ts
@@ -5,22 +5,34 @@ export interface InputTypeInterface {
 	pattern?: string;
 	maxLength?: number;
 	minLength?: number;
+	inputType?: string;
 }
 
-export class InputType implements InputTypeInterface {
-	public title: string;
+const inputTypeMapper: any = {
+	string: 'text',
+	number: 'number'
+};
+
+export class InputType {
+	public label: string;
 	public type: string;
 	public key: string;
 	public pattern?: string;
 	public maxLength?: number;
 	public minLength?: number;
+	public inputType?: string;
 
 	constructor(data: InputTypeInterface) {
-		this.title = data.title;
+		this.label = data.title;
 		this.type = data.type;
 		this.key = data.key;
 		this.pattern = data.pattern;
 		this.maxLength = data.maxLength;
 		this.minLength = data.minLength;
+		this.inputType = data.inputType || this.getTypeInput(this.type);
+	}
+
+	getTypeInput(type: string): string {
+		return inputTypeMapper[type];
 	}
 }
