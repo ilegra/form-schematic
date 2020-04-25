2b58be7e2bc7ab7fc9b8c8b797111da76dbc2370
Adrian Lemes <>
2020-04-25 18:51:27 -0300

    removed generated component

--- example/angular-template/src/app/app.module.ts
+++ example/angular-template/src/app/app.module.ts
@@ -1,14 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
-import { PersonalInformationFormComponent } from './personal-information/personal-information-form.component';
 
 @NgModule({
-	declarations: [AppComponent, PersonalInformationFormComponent],
-	imports: [BrowserModule, SharedModule, ReactiveFormsModule, FormsModule],
+	declarations: [AppComponent],
+	imports: [BrowserModule, SharedModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
--- example/angular-template/src/app/personal-information/personal-information.model.ts
+++
@@ -1,9 +0,0 @@
-export interface PersonalInformationInterface {
-    name: string;
-    password: string;
-    email: string;
-    age: number;
-    nationality: string;
-    cpf: string;
-    phoneNumber: string;
-}
