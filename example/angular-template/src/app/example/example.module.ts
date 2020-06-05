92729fc7d83f5a500f4a7cb2978ccfb5d7f5ce9c
Adrian Caetano <adrianlemess@gmail.com>
2020-06-05 17:37:45 -0300

    Improvements
    - Update example module
    - Add localize module
    - Update profile.json

--- example/angular-template/src/app/example/example.module.ts
+++ example/angular-template/src/app/example/example.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ExampleRoutingModule } from './example-routing.module';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { ContainerComponent } from './container/container.component';
+import { ExampleRoutingModule } from './example-routing.module';
+
 
 
 @NgModule({
   declarations: [ContainerComponent],
   imports: [
     CommonModule,
+    SharedModule,
     ExampleRoutingModule
   ]
 })
